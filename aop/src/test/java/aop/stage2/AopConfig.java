33fdcab95ba5476d0407dbf04053a98b4f4835bc
ulimy <ulim130@gmail.com>
2022-10-15 16:38:36 +0900

    AOP 2단계 - 스프링 AOP에 추가하기

--- aop/src/test/java/aop/stage2/AopConfig.java
+++ aop/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,36 @@
 package aop.stage2;
 
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
 
 @Configuration
 public class AopConfig {
+
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice(final PlatformTransactionManager platformTransactionManager) {
+        return new TransactionAdvice(platformTransactionManager);
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor(final TransactionPointcut transactionPointcut,
+        final TransactionAdvice transactionAdvice) {
+        return new TransactionAdvisor(transactionPointcut, transactionAdvice);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
 
 }
